1a48c5213391b59c986b34140ffec09ad1a995a7
braedenkloke <braedenkloke@gmail.com>
2025-11-17 15:36:44 +0000

    create intersection config object
    Decouples the Json reader from configuring the intersection models.

---
+++ main/include/data_structures/intersection_config.hpp
@@ -0,0 +1,9 @@
+#ifndef INTERSECTION_CONFIG_HPP
+#define INTERSECTION_CONFIG_HPP
+
+struct IntersectionConfig{
+    std::string id;
+
+    explicit IntersectionConfig(std::string id): id(id) {};
+};
+#endif // INTERSECTION_CONFIG_HPP
