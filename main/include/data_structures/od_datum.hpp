dcf73d7ad0b8de1916e852863016d72dcae20dea
gamze <gamzeatli2310@gmail.com>
2025-11-24 01:20:16 -0500

    Update od data structure w/o id

--- main/include/data_structures/od_datum.hpp
+++ main/include/data_structures/od_datum.hpp
@@ -4,13 +4,12 @@
 #include <string>
 
 struct ODDatum{
-    int id;
     std::string origin;
     std::string dest;
     int flowRate;
 
-    explicit ODDatum(int id = 0, std::string origin = "", std::string dest = "", int flowRate = 0): 
-            id(id), origin(origin), dest(dest), flowRate(flowRate) {};
+    explicit ODDatum(std::string origin = "", std::string dest = "", int flowRate = 0): 
+            origin(origin), dest(dest), flowRate(flowRate) {};
 };
 
 #endif // OD_DATUM_HPP
