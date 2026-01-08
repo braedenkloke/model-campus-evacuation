087ecd6bb99b977def9081277f92c12c5ad23b88
braedenkloke <77079888+braedenkloke@users.noreply.github.com>
2026-01-08 11:44:23 -0500

    feat(intersection): route traffic by cycling through OD data (#20)
    * add second out for intersection X01
    
    * select vehicle destination by cycling through OD data
    
    * remove deprecated code

--- main/include/atomic_models/intersection.hpp
+++ main/include/atomic_models/intersection.hpp
@@ -17,23 +17,17 @@ using namespace cadmium;
 struct IntersectionState {
     double sigma;
     std::vector<ODDatum> odData;
-    std::vector<int> validRouteIndices; // OD route indices for this intersection
     bool hasCar;                        // Is there a car waiting in intersection
     Vehicle currentCar;                 // The car currently in the intersection
-    int targetPortIndex;                // Selected port index for this intersection
-    int selectedOdIndex;                // Selected route for that car from OD data (as index)
     std::vector<std::string> outRoads;
 
-    explicit IntersectionState(): sigma(infinity), hasCar(false), targetPortIndex(-1),selectedOdIndex(-1) {} 
+    explicit IntersectionState(): sigma(infinity), hasCar(false) {} 
 
 }; 
 
 #ifndef NO_LOGGING
 std::ostream& operator<<(std::ostream &out, const IntersectionState& state) {
-    return out << "HasCar: " << state.hasCar 
-               << ", IntersectionOutPortIndex: " << state.targetPortIndex
-               << ", SelectedOdDataIndex: " << state.selectedOdIndex;
-
+    return out << "HasCar: " << state.hasCar;
 }
 #endif
 
@@ -46,8 +40,7 @@ public:
     Port<Vehicle> out2; 
     Port<Vehicle> out3; 
     Port<Vehicle> out4;  
-
-    std::vector <Port<Vehicle>> outPorts;
+    bool verbose;   // Prints debugging statements when true.
    
     // ARGUMENTS
     // id - Model name. Equivalent to the origin in the OD data.
@@ -56,32 +49,28 @@ public:
     //            Output ports are allocated to each road name in the order given, i.e., outRoads.first() -> out1.
     Intersection(const std::string id, const std::vector<ODDatum>& odData, const std::vector<std::string>& outRoads): 
                  Atomic<IntersectionState>(id, IntersectionState()) {
+        verbose = false; 
+
         in = addInPort<Vehicle>("in");
         
         out1 = addOutPort<Vehicle>("out1");
         out2 = addOutPort<Vehicle>("out2");
         out3 = addOutPort<Vehicle>("out3");
         out4 = addOutPort<Vehicle>("out4");
-        outPorts = {out1,out2,out3,out4};
 
-        // Store OD data route indices (max 4) that share the same origin/intersection
+        // Filter OD data
         for(int i = 0; i < odData.size(); i++){
             if(odData[i].origin == id){
-                if(state.validRouteIndices.size()< 4){
-                    state.validRouteIndices.push_back(i);
-                }
+                state.odData.push_back(odData[i]);
             }
         }
-        state.odData = odData;
-        assert(outRoads.size() <= outPorts.size());
+        assert(outRoads.size() <= 4);
         state.outRoads = outRoads;
     }
 
     void internalTransition(IntersectionState& state) const override {
         // Wait for next car to enter intersection.
         state.hasCar = false; 
-        state.targetPortIndex = -1;
-        state.selectedOdIndex = -1;
         state.sigma = infinity; 
     }
 
@@ -100,6 +89,7 @@ public:
 
      void output(const IntersectionState& state) const override {
         int o = getOutputPortID(state);
+        if (verbose) {std::cout << this->id << " routing vehicle to out" << o << "\n|";}
         if (o == 1){
             out1->addMessage(state.currentCar);
         } else if (o == 2) {
@@ -117,12 +107,14 @@ public:
 
 private:
     std::string selectDest(const IntersectionState& state) const {
-        bool verbose = false;
+        // Temporary solution: Cycle through OD data
         std::string dest = "";
- 
-        for (ODDatum d : state.odData) {
-            // Assign last destination in odData as target destination.
-            if(verbose) {std::cout << "Assigning dest: " << d.dest << "\n";}
+        static int i = 0;
+        if (verbose) {std::cout << "Select dest i: " << i << "\n";}
+        if (!state.odData.empty()) {
+            i++;
+            if (i >= state.odData.size()) {i = 0;}
+            ODDatum d = state.odData[i];
             dest = d.dest;
         }
         return dest;
@@ -130,7 +122,6 @@ private:
 
     // Returns ID of output port. Returns 0 if no ID found.
     int getOutputPortID(const IntersectionState& state) const {
-        bool verbose = false;
         std::string target = state.currentCar.dest;
         int counter = 0;
         int id = counter;
@@ -143,23 +134,6 @@ private:
             }
         }
         return id;
-    }
-
-    int selectRouteWithMaxFlow(const IntersectionState& state) const {
-        int bestPortIndex = -1;
-        int maxFlow = -1;
-
-        // Select the route with the highest flow rate for this intersection
-        for(size_t i = 0; i < state.validRouteIndices.size(); i++) {
-            int odIndex = state.validRouteIndices[i];
-            int currentFlow = state.odData[odIndex].flowRate;
-
-            if(currentFlow > maxFlow) {
-             maxFlow = currentFlow;
-             bestPortIndex = i;
-            }
-        }
-        return bestPortIndex; 
     }
 };
 
--- main/include/coupled_models/carleton_university_campus.hpp
+++ main/include/coupled_models/carleton_university_campus.hpp
@@ -17,19 +17,28 @@ struct CarletonUniversityCampusCoupled : public Coupled {
 
     CarletonUniversityCampusCoupled(const std::string& id, std::vector<int> carDepartureTimes, 
                                     const std::vector<ODDatum>& odData ): Coupled(id) {
+        // Road names, as per documentation in the final report.
+        std::string r01Name = "Library Rd & P1 to Library Rd & University Dr";
+        std::string r02Name = "Library Rd & P1 to Campus Ave & Library Rd";
+
         // Out roads
-        std::vector<std::string> x01OutRoads{"Library Rd & P1 to Library Rd & University Dr"};
+        std::vector<std::string> noOutRoads = {};
+        std::vector<std::string> x01OutRoads = {r01Name, r02Name};
 
         // Create models
         auto p1 = addComponent<ParkingLot>("P1", carDepartureTimes);
         auto x01 = addComponent<Intersection>("Library Rd & P1", odData, x01OutRoads);
-        auto r01 = addComponent<Road>("Library Rd & P1 to Library Rd & University Dr");
+        auto x02 = addComponent<Intersection>("Library Rd & University Dr", odData, noOutRoads);
+        auto x03 = addComponent<Intersection>("Campus Ave & Library Rd", odData, noOutRoads);
+        auto r01 = addComponent<Road>(r01Name);
+        auto r02 = addComponent<Road>(r02Name);
 
         // Couple intersection inputs
         addCoupling(p1->exit, x01->in);
 
         // Couple intersection outputs
         addCoupling(x01->out1, r01->entrance);
+        addCoupling(x01->out2, r02->entrance);
     }
 };
 #endif // CARLETON_UNIVERSITY_CAMPUS_HPP
