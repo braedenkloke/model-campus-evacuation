2ad28c48c5e4f2569df6ec0465ba111e21dea9fe
Braeden Kloke <braedenkloke@gmail.com>
2026-03-05 14:40:37 -0500

    create scenario 1 animation

--- run_scenarios.sh
+++ run_scenarios.sh
@@ -9,6 +9,7 @@ declare -a scenarios=(
 for s in "${scenarios[@]}"; do
     ./bin/campus_evacuation -i input_data/parking_lot_schedules/${s}.csv -o output_data/raw/${s}_log.csv
     python analysis/data_analysis.py output_data/raw/${s}_log.csv
+    cp -v output_data/processed/heatmap_matrix.csv output_data/processed/${s}_heatmap_matrix.csv
     python analysis/visualize_processed.py output_data/processed
     cp -v output_data/processed/heatmap_matrix.png output_data/processed/${s}_heatmap.png
     done
